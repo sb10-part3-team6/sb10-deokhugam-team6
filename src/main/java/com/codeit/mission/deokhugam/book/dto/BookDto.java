04f09d05c01902a8159f893cfe5b9217a64552f6
초시맥북 <fyg7575@gmail.com>
2026-04-15 17:29:57 +0900

    feat: book 도메인 설계

---
+++ src/main/java/com/codeit/mission/deokhugam/book/dto/BookDto.java
@@ -0,0 +1,21 @@
+package com.codeit.mission.deokhugam.book.dto;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record BookDto(
+    UUID id,
+    String title,
+    String author,
+    String description,
+    String publisher,
+    LocalDate publishedDate,
+    String isbn,
+    String thumbnailUrl,
+    long reviewCount,
+    double rating,
+    LocalDateTime createdAt,
+    LocalDateTime updatedAt
+) {
+}
