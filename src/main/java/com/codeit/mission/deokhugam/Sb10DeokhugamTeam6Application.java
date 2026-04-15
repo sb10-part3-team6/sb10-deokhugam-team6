e6477b3620b31ecba8ff4d4f2bc5870f1be9c965
CHH01 <alex011003@gmail.com>
2026-04-15 10:34:13 +0900

    chore: 프로젝트 초기 뼈대 및 패키지 구조 설정
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/com/codeit/mission/deokhugam/Sb10DeokhugamTeam6Application.java
@@ -0,0 +1,13 @@
+package com.codeit.mission.deokhugam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb10DeokhugamTeam6Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Sb10DeokhugamTeam6Application.class, args);
+  }
+
+}
---
+++ src/test/java/com/codeit/mission/deokhugam/Sb10DeokhugamTeam6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeit.mission.deokhugam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb10DeokhugamTeam6ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
