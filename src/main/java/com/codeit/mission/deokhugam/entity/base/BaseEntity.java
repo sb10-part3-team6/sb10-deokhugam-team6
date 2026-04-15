23bc2b474a89604416af3d519caba26fa5a26c32
LeeDyol <dlekthf819203@naver.com>
2026-04-15 11:29:01 +0900

    Feat: 공통 엔티티 BaseEntity 구현

---
+++ src/main/java/com/codeit/mission/deokhugam/entity/base/BaseEntity.java
@@ -0,0 +1,31 @@
+package com.codeit.mission.deokhugam.entity.base;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.sql.Timestamp;
+
+/*
+    공통 엔티티
+ */
+@Getter
+@NoArgsConstructor
+@MappedSuperclass                                       // 하위 클래스에도 자동 기록 적용
+@EntityListeners(AuditingEntityListener.class)          // 객체 생성 / 수정 시점 자동 기록
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)     // 무작위 UUID 발급
+    private Long id;                                    // 도매안 식별자
+
+    @CreatedDate
+    @Column(updatable = false)
+    private Timestamp createdAt;                        // 객체 생성 시점
+
+    @LastModifiedDate
+    private Timestamp updatedAt;                        // 객체 수정 시점
+}
