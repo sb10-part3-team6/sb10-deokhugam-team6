18df83bbccabe5a87c5a447baa75e6f8f7e2d95a
초시맥북 <fyg7575@gmail.com>
2026-04-15 17:29:57 +0900

    feat: book 도메인 설계

---
+++ src/main/java/com/codeit/mission/deokhugam/book/entity/Book.java
@@ -0,0 +1,43 @@
+package com.codeit.mission.deokhugam.book.entity;
+
+import com.codeit.mission.deokhugam.base.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Setter
+@Getter
+public class Book extends BaseEntity {
+    @Column(nullable = false)
+    String title;
+
+    @Column(nullable = false)
+    String author;
+
+    @Column(nullable = false)
+    String description;
+
+    @Column(nullable = false)
+    String publisher;
+
+    @Column(nullable = false)
+    LocalDate publishedDate;
+
+    @Column(nullable = false)
+    String isbn;
+
+    @Column(nullable = false)
+    String thumbnailUrl;
+
+    @Column(nullable = false)
+    long reviewCount;
+
+    @Column(nullable = false)
+    BigDecimal rating;
+
+}
