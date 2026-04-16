692b91e612b07556c859f3d9ab103697f8b615f2
LeeDyol <dlekthf819203@naver.com>
2026-04-16 10:59:35 +0900

    Feat: MapStruct를 활용한 ReviewMapper 정의

---
+++ src/main/java/com/codeit/mission/deokhugam/review/mapper/ReviewMapper.java
@@ -0,0 +1,15 @@
+package com.codeit.mission.deokhugam.review.mapper;
+
+import com.codeit.mission.deokhugam.review.dto.response.ReviewDto;
+import com.codeit.mission.deokhugam.review.entity.Review;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+
+/*
+    리뷰 매퍼
+ */
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface ReviewMapper {
+    // Entity -> 응답 DTO 변환
+    ReviewDto toDto(Review review);
+}
