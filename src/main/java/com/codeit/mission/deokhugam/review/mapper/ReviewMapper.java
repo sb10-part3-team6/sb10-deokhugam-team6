8a2d24f63115df9d0b2a3b870b33f087aac96870
LeeDyol <dlekthf819203@naver.com>
2026-04-16 13:08:25 +0900

    Refactor: ReviewMapper에 좋아요 여부 매핑 로직 추가

--- src/main/java/com/codeit/mission/deokhugam/review/mapper/ReviewMapper.java
+++ src/main/java/com/codeit/mission/deokhugam/review/mapper/ReviewMapper.java
@@ -3,6 +3,7 @@ package com.codeit.mission.deokhugam.review.mapper;
 import com.codeit.mission.deokhugam.review.dto.response.ReviewDto;
 import com.codeit.mission.deokhugam.review.entity.Review;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 
 /*
@@ -11,5 +12,6 @@ import org.mapstruct.MappingConstants;
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface ReviewMapper {
     // Entity -> 응답 DTO 변환
+    @Mapping(target = "likedByMe", source = "isLiked")
     ReviewDto toDto(Review review);
 }
