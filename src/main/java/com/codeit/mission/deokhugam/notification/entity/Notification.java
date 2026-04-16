dfa259693d6079d1f20e3f15cc74692aee7b1947
Nooroong <sjiyeon759@gmail.com>
2026-04-16 17:26:23 +0900

    Feat: Notification 엔티티에 Builder 패턴 추가

--- src/main/java/com/codeit/mission/deokhugam/notification/entity/Notification.java
+++ src/main/java/com/codeit/mission/deokhugam/notification/entity/Notification.java
@@ -10,6 +10,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -32,12 +33,22 @@ public class Notification extends BaseEntity {
     @JoinColumn(name = "user_id")
     private User user; // 알림을 수신할 사용자의 ID
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "review_id")
-  private Review review; // 좋아요 또는 댓글이 달린 리뷰
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id")
+    private Review review; // 알림 대상이 된 리뷰(좋아요, 댓글, 랭킹)
 
-  public void updateConfirmed(boolean confirmed) {
-    this.confirmed = confirmed;
-  }
+    @Builder
+    public Notification(String reviewContent, String message, boolean confirmed, User user,
+        Review review) {
+        this.reviewContent = reviewContent;
+        this.message = message;
+        this.confirmed = confirmed;
+        this.user = user;
+        this.review = review;
+    }
+
+    public void updateConfirmed(boolean confirmed) {
+        this.confirmed = confirmed;
+    }
 
 }
