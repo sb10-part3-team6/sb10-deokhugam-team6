91f0f6f4c274b1ea68748b1d6bc9f4db530f3292
LeeDyol <dlekthf819203@naver.com>
2026-04-16 14:30:28 +0900

    Refactor: ReviewController 경로 매핑 최적화

--- src/main/java/com/codeit/mission/deokhugam/review/controller/ReviewController.java
+++ src/main/java/com/codeit/mission/deokhugam/review/controller/ReviewController.java
@@ -9,14 +9,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /*
     리뷰 컨트롤러
  */
 @RestController
-@RequestMapping("/api/reviews")
 @RequiredArgsConstructor
 public class ReviewController {
     private final ReviewService reviewService;
@@ -25,7 +23,7 @@ public class ReviewController {
     // 리뷰 목록 조회
 
     // 리뷰 등록
-    @PostMapping
+    @PostMapping("/api/reviews")
     public ResponseEntity<ReviewDto> create(@Valid @RequestBody ReviewCreateRequest reviewCreateRequest) {
         ReviewDto reviewDto = reviewService.create(reviewCreateRequest);
 
