8dd457580dc45e79ddaf2e2f0455deeed4371410
LeeDyol <dlekthf819203@naver.com>
2026-04-16 13:07:59 +0900

    Refactor: 리뷰 생성 로직 개선 및 엔티티 객체 참조 적용

--- src/main/java/com/codeit/mission/deokhugam/review/service/ReviewServiceImplement.java
+++ src/main/java/com/codeit/mission/deokhugam/review/service/ReviewServiceImplement.java
@@ -1,5 +1,6 @@
 package com.codeit.mission.deokhugam.review.service;
 
+import com.codeit.mission.deokhugam.book.entity.Book;
 import com.codeit.mission.deokhugam.error.ErrorCode;
 import com.codeit.mission.deokhugam.review.dto.request.ReviewCreateRequest;
 import com.codeit.mission.deokhugam.review.dto.response.ReviewDto;
@@ -7,6 +8,8 @@ import com.codeit.mission.deokhugam.review.entity.Review;
 import com.codeit.mission.deokhugam.review.exception.DuplicateReviewException;
 import com.codeit.mission.deokhugam.review.mapper.ReviewMapper;
 import com.codeit.mission.deokhugam.review.repository.ReviewRepository;
+import com.codeit.mission.deokhugam.user.entity.User;
+import com.codeit.mission.deokhugam.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,6 +25,9 @@ import java.util.UUID;
 @Transactional(readOnly = true)
 public class ReviewServiceImplement implements ReviewService {
     private final ReviewRepository reviewRepository;
+    private final BookRepository bookRepository;
+    private final UserRepository userRepository;
+    private final LikeRepository likeRepository;
 
     private final ReviewMapper reviewMapper;
 
@@ -32,17 +38,34 @@ public class ReviewServiceImplement implements ReviewService {
         // 1. 중복 검사
         validateDuplicateReview(reviewCreateRequest.bookId(), reviewCreateRequest.userId());
 
-        // 2. 리뷰 생성
+        // 2. Book / User 엔티티 조회
+        Book book = getBookEntityOrThrow(reviewCreateRequest.bookId());
+        User user = getUserEntityOrThrow(reviewCreateRequest.userId());
+
+        // 3. 리뷰 생성
         Review newReview = Review.builder()
-                .bookId(reviewCreateRequest.bookId())
-                .userId(reviewCreateRequest.userId())
+                .book(book)
+                .user(user)
                 .content(reviewCreateRequest.content())
                 .rating(reviewCreateRequest.rating())
                 .build();
         reviewRepository.save(newReview);
 
-        // 3. 리뷰 응답 DTO 변환 및 반환
-        return reviewMapper.toDto(newReview);
+        // 4. 리뷰 응답 DTO 변환 및 반환
+        boolean isLiked = likeRepository.existByReviewIdAndUserId(newReview.getId(), newReview.getUser().getId());
+        return reviewMapper.toDto(newReview, isLiked);
+    }
+
+    // Book 엔티티 반환
+    private Book getBookEntityOrThrow(UUID bookId) {
+        return bookRepository.findById(bookId)
+                .orElseThrow();
+    }
+
+    // User 엔티티 반환
+    private User getUserEntityOrThrow(UUID userId) {
+        return userRepository.findById(userId)
+                .orElseThrow();
     }
 
     // 유효성 검증 (중복 검사): 사용자가 이미 특정 도서에 리뷰를 남긴 경우, 예외 발생
