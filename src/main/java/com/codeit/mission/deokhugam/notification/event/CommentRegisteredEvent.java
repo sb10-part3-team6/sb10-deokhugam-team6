41c5001f16ea325fee3b6d94975c56d364d8caff
Nooroong <sjiyeon759@gmail.com>
2026-04-17 10:25:09 +0900

    Comment: 코드의 의미와 일치하도록 주석을 올바르게 수정

--- src/main/java/com/codeit/mission/deokhugam/notification/event/CommentRegisteredEvent.java
+++ src/main/java/com/codeit/mission/deokhugam/notification/event/CommentRegisteredEvent.java
@@ -13,6 +13,6 @@ public class CommentRegisteredEvent {
 
     private UUID actorId; // 댓글을 작성한 유저의 id
     private UUID receiverId; // 알림을 받을 유저의 id
-    private UUID reviewId; // 랭킹에 선정된 리뷰의 id
+    private UUID reviewId; // 댓글이 등록된 리뷰의 id
 
 }
