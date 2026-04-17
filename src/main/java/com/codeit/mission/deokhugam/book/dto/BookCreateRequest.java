e04693fa0718cfd542be6307a0900a663f3cf3d9
초시맥북 <fyg7575@gmail.com>
2026-04-17 10:23:50 +0900

    fix: 조건에 해당될 시 출력될 메시지 작성

--- src/main/java/com/codeit/mission/deokhugam/book/dto/BookCreateRequest.java
+++ src/main/java/com/codeit/mission/deokhugam/book/dto/BookCreateRequest.java
@@ -7,23 +7,23 @@ import java.time.LocalDate;
 
 public record BookCreateRequest(
         //제목
-        @NotBlank
+        @NotBlank(message = "제목을 입력해주세요")
         String title,
 
         //지은이
-        @NotBlank
+        @NotBlank(message = "지은이를 입력해주세요.")
         String author,
 
         //책에 대한 설명
-        @NotBlank
+        @NotBlank(message = "설명을 최소 1글자 이상 적어주세요.")
         String description,
 
         //출판사
-        @NotBlank
+        @NotBlank(message = "출판사 정보를 입력해주세요.")
         String publisher,
 
         //출판일
-        @NotNull
+        @NotNull(message = "출판 일자를 적어주세요.")
         LocalDate publishedDate,
 
         //ISBN 값
