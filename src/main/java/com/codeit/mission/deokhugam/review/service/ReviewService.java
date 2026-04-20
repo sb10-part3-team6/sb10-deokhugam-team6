d86b2cc6d818a5a45e9e4a8b4cc6a60a4e6ac40e
LeeDyol <dlekthf819203@naver.com>
2026-04-20 11:46:09 +0900

    Feat: 리뷰 관리 (삭제 / 좋아요) 관련 추상 메서드 정의

--- src/main/java/com/codeit/mission/deokhugam/review/service/ReviewService.java
+++ src/main/java/com/codeit/mission/deokhugam/review/service/ReviewService.java
@@ -18,4 +18,13 @@ public interface ReviewService {
 
     // 리뷰 수정
     ReviewDto update(UUID id, UUID requestUserId, ReviewUpdateRequest reviewUpdateRequest);
+
+    // 리뷰 논리 삭제
+    void logicalDelete(UUID id, UUID requestUserId);
+
+    // 리뷰 물리 삭제
+    void physicalDelete(UUID id, UUID requestUserId);
+
+    // 리뷰 좋아요
+    ReviewLikeDto createReviewLike(UUID id, UUID requestUserId);
 }
