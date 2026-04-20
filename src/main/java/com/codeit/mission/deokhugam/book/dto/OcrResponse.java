73441ec04bc228f0b121e54d16ee64e4960bad61
초시맥북 <fyg7575@gmail.com>
2026-04-20 18:42:02 +0900

    fix: dto 내부 클래스의 클래스명을 컨벤션에 맞게 수정

--- src/main/java/com/codeit/mission/deokhugam/book/dto/OcrResponse.java
+++ src/main/java/com/codeit/mission/deokhugam/book/dto/OcrResponse.java
@@ -5,9 +5,9 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
 public record OcrResponse(
-        @JsonProperty("ParsedResults") List<parsedResult> parsedResults
+        @JsonProperty("ParsedResults") List<ParsedResult> parsedResults
 ) {
-    public record parsedResult(
+    public record ParsedResult(
             @JsonProperty("ParsedText") String parsedText
     ) {}
 }
