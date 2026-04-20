952b20a1bcfcb8e1758eecb3dd4b8b1f0a20598c
Nooroong <sjiyeon759@gmail.com>
2026-04-20 16:52:01 +0900

    Feat: 커스텀 레포지토리 작성

---
+++ src/main/java/com/codeit/mission/deokhugam/notification/repository/custom/NotificationRepositoryImpl.java
@@ -0,0 +1,83 @@
+package com.codeit.mission.deokhugam.notification.repository.custom;
+
+import com.codeit.mission.deokhugam.dashboard.DirectionEnum;
+import com.codeit.mission.deokhugam.notification.dto.NotificationRequestQuery;
+import com.codeit.mission.deokhugam.notification.entity.Notification;
+import com.codeit.mission.deokhugam.notification.entity.QNotification;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
+
+@RequiredArgsConstructor
+public class NotificationRepositoryImpl implements NotificationRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+    QNotification notification = QNotification.notification;
+
+    @Override
+    public Slice<Notification> findByUserWithCursor(UUID userId, NotificationRequestQuery query) {
+
+        int limit = query.getLimitOrDefault();
+
+        BooleanBuilder builder = new BooleanBuilder();
+        builder.and(notification.user.id.eq(userId));
+
+        // after (createdAt 기준 커서)
+        if (query.after() != null) {
+            if (isDesc(query)) {
+                builder.and(notification.createdAt.lt(query.after()));
+            } else {
+                builder.and(notification.createdAt.gt(query.after()));
+            }
+        }
+
+        // cursor
+        if (query.cursor() != null) {
+            LocalDateTime cursorTime = LocalDateTime.parse(query.cursor());
+
+            if (isDesc(query)) {
+                builder.and(notification.createdAt.lt(cursorTime));
+            } else {
+                builder.and(notification.createdAt.gt(cursorTime));
+            }
+        }
+
+        List<Notification> result = queryFactory
+            .selectFrom(notification)
+            .where(builder)
+            .orderBy(getOrderSpecifier(query))
+            .limit(limit + 1)
+            .fetch();
+
+        boolean hasNext = result.size() > limit;
+
+        if (hasNext) {
+            result = result.subList(0, limit);
+        }
+
+        return new SliceImpl<>(result, PageRequest.of(0, limit), hasNext);
+    }
+
+    @Override
+    public long countByUserId(UUID userId) {
+        return 0;
+    }
+
+    private boolean isDesc(NotificationRequestQuery query) {
+        return query.direction() == null || query.direction().equals(DirectionEnum.DESC);
+    }
+
+    private OrderSpecifier<?> getOrderSpecifier(NotificationRequestQuery query) {
+        return isDesc(query)
+            ? notification.createdAt.desc()
+            : notification.createdAt.asc();
+    }
+
+}
