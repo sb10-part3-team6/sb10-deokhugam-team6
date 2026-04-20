b674b8cb83644da16ae0f1781bfdcd0fb05a7192
Nooroong <sjiyeon759@gmail.com>
2026-04-20 16:19:27 +0900

    Feat: NotificationRepository 작성

--- src/main/java/com/codeit/mission/deokhugam/notification/repository/NotificationRepository.java
+++ src/main/java/com/codeit/mission/deokhugam/notification/repository/NotificationRepository.java
@@ -1,9 +1,12 @@
 package com.codeit.mission.deokhugam.notification.repository;
 
 import com.codeit.mission.deokhugam.notification.entity.Notification;
+import com.codeit.mission.deokhugam.notification.repository.custom.NotificationRepositoryCustom;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface NotificationRepository extends JpaRepository<Notification, UUID> {
+public interface NotificationRepository extends JpaRepository<Notification, UUID>,
+    NotificationRepositoryCustom {
+
 
 }
