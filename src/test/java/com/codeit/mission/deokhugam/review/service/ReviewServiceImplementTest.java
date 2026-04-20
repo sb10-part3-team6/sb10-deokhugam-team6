c18526746eb9546e2725bc9e1194385fc0aa9b44
LeeDyol <dlekthf819203@naver.com>
2026-04-20 15:10:22 +0900

    Test: 리뷰 논리 삭제 (Soft Delete) 성공 단위 테스트 작성

--- src/test/java/com/codeit/mission/deokhugam/review/service/ReviewServiceImplementTest.java
+++ src/test/java/com/codeit/mission/deokhugam/review/service/ReviewServiceImplementTest.java
@@ -334,4 +334,43 @@ public class ReviewServiceImplementTest {
         verify(reviewRepository, never()).save(any(Review.class));                      // Repository의 save 함수 미호출 확인
         verify(reviewMapper, never()).toDto(any(Review.class), anyBoolean());           // Mapper의 toDto 함수 미호출 확인
     }
+
+    /*
+        리뷰 삭제
+     */
+
+    // [논리 삭제 성공]
+    @Test
+    @DisplayName("리뷰 논리 삭제 성공")
+    void delete_review_success() {
+        // given
+        UUID reviewId = UUID.randomUUID();
+        UUID userId = UUID.randomUUID();
+
+        // 가짜 객체 | 도서 및 사용자
+        Book mockBook = Book.builder().build();
+        User mockUser = User.builder().build();
+        ReflectionTestUtils.setField(mockUser, "id", userId);                                              // NPE 방지를 위한 id 강제 삽입
+
+        // 삭제할 리뷰 정보
+        Review savedReview = Review.builder()
+                .book(mockBook)
+                .user(mockUser)
+                .content("돌덩이 외게인이 뭐가 재밌다고 난리야")
+                .rating(3)
+                .build();
+        ReflectionTestUtils.setField(savedReview, "id", reviewId);                                         // NPE 방지를 위한 id 강제 주입
+        ReflectionTestUtils.setField(savedReview, "status", ReviewStatus.ACTIVE);                          // status 강제 주입
+
+
+        given(reviewRepository.findById(reviewId)).willReturn(Optional.of(savedReview));                        // savedReview 반환
+        given(userRepository.findById(userId)).willReturn(Optional.of(mockUser));                               // mockUser 반환
+
+        // when
+        reviewServiceImplement.delete(reviewId, userId);
+
+        // then
+        assertEquals(ReviewStatus.DELETED, savedReview.getStatus());                // 특정 리뷰의 논리 삭제 여부 검증
+        verify(reviewRepository, never()).delete(any(Review.class));                // Repository의 delete 함수 미호출 확인
+    }
 }
