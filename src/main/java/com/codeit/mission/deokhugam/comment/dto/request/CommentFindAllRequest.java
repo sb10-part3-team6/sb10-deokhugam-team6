cfad417ecbf9a9df56f27264c30defd1ec042d43
rorm0819 <rorm0819@gmail.com>
2026-04-22 08:40:23 +0900

    Fix: pr#127 코드래빗 수정 권고 반영4

--- src/main/java/com/codeit/mission/deokhugam/comment/dto/request/CommentFindAllRequest.java
+++ src/main/java/com/codeit/mission/deokhugam/comment/dto/request/CommentFindAllRequest.java
@@ -3,6 +3,7 @@ package com.codeit.mission.deokhugam.comment.dto.request;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -10,11 +11,13 @@ import java.util.UUID;
 public record CommentFindAllRequest(
         @NotNull
         UUID reviewId,
+
+        @Pattern(regexp = "^(ASC|DESC)$", message = "direction은 ASC 또는 DESC만 가능합니다.")
         String direction,
         String cursor,
         LocalDateTime after,
 
-        @Min(value = 1, message = "값은 최소 1 이상이어햐 합니다.")
+        @Min(value = 1, message = "값은 최소 1 이상이어야 합니다.")
         @Max(value = 100, message = "값은 최대 100 이하여야 합니다.")
         Integer limit
 ) {
