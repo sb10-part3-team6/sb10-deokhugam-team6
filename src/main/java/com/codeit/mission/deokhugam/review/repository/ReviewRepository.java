7f93cf4479ad6b29b2faea3e7488ec220aaf8a4f
LeeDyol <dlekthf819203@naver.com>
2026-04-21 13:50:55 +0900

    Style: 불필요한 주석 및 미사용 코드 제거

--- src/main/java/com/codeit/mission/deokhugam/review/repository/ReviewRepository.java
+++ src/main/java/com/codeit/mission/deokhugam/review/repository/ReviewRepository.java
@@ -42,12 +42,10 @@ public interface ReviewRepository extends JpaRepository<Review, UUID> {
   int deleteReviewLike(@Param("reviewId") UUID reviewId, @Param("userId") UUID userId);
 
   // 특정 리뷰에 대한 특정 유저의 좋아요 여부
-  @Query("SELECT COUNT(review.id) > 0 " +// 조건 만족 여부에 따라, true / false 반환
+  @Query("SELECT COUNT(review.id) > 0 " +                       // 조건 만족 여부에 따라, true / false 반환
       "FROM Review review " +
-      "JOIN review.likedUsers user " +
-      // Review 엔티티 내부 likedUser 필드 조인
+      "JOIN review.likedUsers user " +                          // Review 엔티티 내부 likedUser 필드 조인
       "WHERE review.id = :reviewId AND user.id = :userId")
-  // 리뷰 id 및 사용자 id에 대한 완전 일치 조건
   boolean existsLikedByIdAndUserId(@Param("reviewId") UUID reviewId,
       @Param("userId") UUID userId);
 
