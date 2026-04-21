3e22337173de135528f268214934fe83cf21fcfc
LeeDyol <dlekthf819203@naver.com>
2026-04-21 14:36:07 +0900

    Feat: QueryDSL 적용을 위한 ReviewRepository 커스텀 인터페이스 추가

--- src/main/java/com/codeit/mission/deokhugam/review/repository/ReviewRepository.java
+++ src/main/java/com/codeit/mission/deokhugam/review/repository/ReviewRepository.java
@@ -17,7 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
     리뷰 레파지토리
  */
 @Repository
-public interface ReviewRepository extends JpaRepository<Review, UUID> {
+public interface ReviewRepository extends JpaRepository<Review, UUID>, ReviewRepositoryCustom {
 
   // 중복 검사: 특정 도서에 대한 사용자 리뷰 존재 유무
   boolean existsByBookIdAndUserId(UUID bookId, UUID userId);
