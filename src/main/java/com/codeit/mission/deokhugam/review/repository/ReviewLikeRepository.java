69f8d9760785fb33b5c064c77b0deedda38f0878
LeeDyol <dlekthf819203@naver.com>
2026-04-21 17:52:16 +0900

    Feat: Review ID와 User ID 기반의 ReviewLike 조회 메서드 추가

--- src/main/java/com/codeit/mission/deokhugam/review/repository/ReviewLikeRepository.java
+++ src/main/java/com/codeit/mission/deokhugam/review/repository/ReviewLikeRepository.java
@@ -2,6 +2,7 @@ package com.codeit.mission.deokhugam.review.repository;
 
 import com.codeit.mission.deokhugam.dashboard.users.dto.PowerUserLikeCount;
 import com.codeit.mission.deokhugam.review.entity.ReviewLike;
+import com.codeit.mission.deokhugam.user.entity.User;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
@@ -12,21 +13,24 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReviewLikeRepository extends JpaRepository<ReviewLike, UUID> {
+
   @Query(
       """
-      select new com.codeit.mission.deokhugam.dashboard.users.dto.PowerUserLikeCount(
-          rl.user.id,
-          count(rl)
-      )
-      from ReviewLike rl
-      where rl.review.status = com.codeit.mission.deokhugam.review.entity.ReviewStatus.ACTIVE
-            and rl.likedAt >= :periodStart
-            and rl.likedAt < :periodEnd
-      group by rl.user.id
-      """
+          select new com.codeit.mission.deokhugam.dashboard.users.dto.PowerUserLikeCount(
+              rl.user.id,
+              count(rl)
+          )
+          from ReviewLike rl
+          where rl.review.status = com.codeit.mission.deokhugam.review.entity.ReviewStatus.ACTIVE
+                and rl.likedAt >= :periodStart
+                and rl.likedAt < :periodEnd
+          group by rl.user.id
+          """
   )
   List<PowerUserLikeCount> findUserLikeCounts(
       @Param("periodStart") LocalDateTime periodStart,
       @Param("periodEnd") LocalDateTime periodEnd);
 
+  // 리뷰 ID 및 사용자 ID를 통한 리뷰 좋아요 조회
+  ReviewLike findByReviewIdAndUserId(UUID reviewId, UUID userId);
 }
