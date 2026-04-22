414841e50838e30abf019de0111249e379a3dcd3
Nooroong <sjiyeon759@gmail.com>
2026-04-22 16:21:08 +0900

    Feat: findByUserId() 쿼리메서드 추가
    성# 본문에는 변경한 내용과 이유 설명 (어떻게보다는 무엇 & 왜를 설명)

--- src/main/java/com/codeit/mission/deokhugam/notification/repository/NotificationRepository.java
+++ src/main/java/com/codeit/mission/deokhugam/notification/repository/NotificationRepository.java
@@ -1,8 +1,8 @@
 package com.codeit.mission.deokhugam.notification.repository;
 
 import com.codeit.mission.deokhugam.notification.entity.Notification;
-import java.util.List;
 import com.codeit.mission.deokhugam.notification.repository.custom.NotificationRepositoryCustom;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -14,15 +14,17 @@ public interface NotificationRepository extends JpaRepository<Notification, UUID
     NotificationRepositoryCustom {
 
 
-  // 사용자와 관련된 모든 알림을 일괄 삭제
-  @Modifying
-  @Transactional
-  @Query(value = "DELETE FROM notifications WHERE user_id IN :userIds", nativeQuery = true)
-  void deleteByUserIds(@Param("userIds") List<UUID> userIds);
+    // 사용자와 관련된 모든 알림을 일괄 삭제
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM notifications WHERE user_id IN :userIds", nativeQuery = true)
+    void deleteByUserIds(@Param("userIds") List<UUID> userIds);
 
-  // 삭제 대상 유저들이 작성한 리뷰와 관련된 모든 알림을 일괄 삭제 (서브쿼리 활용)
-  @Modifying
-  @Transactional
-  @Query(value = "DELETE FROM notifications WHERE review_id IN (SELECT id FROM reviews WHERE user_id IN :userIds)", nativeQuery = true)
-  void deleteByReviewUserIds(@Param("userIds") List<UUID> userIds);
+    // 삭제 대상 유저들이 작성한 리뷰와 관련된 모든 알림을 일괄 삭제 (서브쿼리 활용)
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM notifications WHERE review_id IN (SELECT id FROM reviews WHERE user_id IN :userIds)", nativeQuery = true)
+    void deleteByReviewUserIds(@Param("userIds") List<UUID> userIds);
+
+    List<Notification> findByUserId(UUID userId);
 }
