437804e949025689871f073b3153741627df5371
LeeDyol <dlekthf819203@naver.com>
2026-04-22 11:46:01 +0900

    Refactor: CursorPageResponseReviewDto 제네릭 타입 활성화 및 재사용성 개선

--- src/main/java/com/codeit/mission/deokhugam/review/dto/response/CursorPageResponseReviewDto.java
+++ src/main/java/com/codeit/mission/deokhugam/review/dto/response/CursorPageResponseReviewDto.java
@@ -9,12 +9,13 @@ import java.util.List;
     리뷰 페이지네이션 응답 DTO
  */
 @Builder
-public record CursorPageResponseReviewDto<T> (
-        List<ReviewDto> content,                // 실제 데이터 목록
-        String nextCursor,                      // 다음 페이지 커서
-        LocalDateTime nextAfter,                // 이전 페이지의 마지막 요소 생성 시간
-        int size,                               // 페이지 크기
-        long totalElements,                     // 총 요소 수
-        boolean hasNext                         // 다음 페이지 여부
-){
+public record CursorPageResponseReviewDto<T>(
+    List<T> content,                        // 실제 데이터 목록
+    String nextCursor,                      // 다음 페이지 커서
+    LocalDateTime nextAfter,                // 이전 페이지의 마지막 요소 생성 시간
+    int size,                               // 페이지 크기
+    long totalElements,                     // 총 요소 수
+    boolean hasNext                         // 다음 페이지 여부
+) {
+
 }
