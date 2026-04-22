cdf1801516a3a399ece3f778a7e688614b6a3338
초시맥북 <fyg7575@gmail.com>
2026-04-22 11:44:06 +0900

    fix: 에러코드 오탈자 수정

--- src/main/java/com/codeit/mission/deokhugam/book/exception/S3UrlParseFailureException.java
+++ src/main/java/com/codeit/mission/deokhugam/book/exception/S3UrlParseFailureException.java
@@ -5,6 +5,6 @@ import com.codeit.mission.deokhugam.error.ErrorCode;
 
 public class S3UrlParseFailureException extends DeokhugamException {
     public S3UrlParseFailureException(){
-        super(ErrorCode.S3_URL_PARSH_FAILED);
+        super(ErrorCode.S3_URL_PARSE_FAILED);
     }
 }
