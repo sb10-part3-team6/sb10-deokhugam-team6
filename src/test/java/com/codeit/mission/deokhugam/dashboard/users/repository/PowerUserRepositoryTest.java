c6edaebf69303f4743a4643a374daa7196c84b25
conradrado <gimmy6707@gmail.com>
2026-04-23 17:43:32 +0900

    Refactor: Dashboard 패키지 경로 변경 관련 수정 - Dashboard 내 패키지 이름을 바꿨습니다. - 해당 패키지들을 참조하고 있는 부분의 경로를 수정하였습니다. - PowerUser 테스트 코드도 올바른 패키지를 참조하도록 수정하였습니다.

--- src/test/java/com/codeit/mission/deokhugam/dashboard/users/repository/PowerUserRepositoryTest.java
+++ src/test/java/com/codeit/mission/deokhugam/dashboard/users/repository/PowerUserRepositoryTest.java
@@ -2,6 +2,7 @@ package com.codeit.mission.deokhugam.dashboard.users.repository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import com.codeit.mission.deokhugam.config.QuerydslConfig;
 import com.codeit.mission.deokhugam.dashboard.PeriodType;
 import com.codeit.mission.deokhugam.dashboard.powerusers.dto.PowerUserDto;
 import com.codeit.mission.deokhugam.dashboard.powerusers.entity.PowerUser;
@@ -15,9 +16,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.PageRequest;
 
 @DataJpaTest
+@Import(QuerydslConfig.class)
 class PowerUserRepositoryTest {
 
   private static final UUID SNAPSHOT_ID =
