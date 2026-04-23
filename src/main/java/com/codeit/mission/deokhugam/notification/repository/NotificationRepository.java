669376f1f9397fc8ac5004edc55baa7542a0edf8
Nooroong <sjiyeon759@gmail.com>
2026-04-23 11:02:47 +0900

    Feat: 테스트를 위해 updateAllAsConfirmed의 리턴타입 변경
    - 테스트에서 update가 발생된 레코드의 수를 파악할 수 있도록 리턴타입을 void에서 int로 변경하였습니다.

--- src/main/java/com/codeit/mission/deokhugam/notification/repository/NotificationRepository.java
+++ src/main/java/com/codeit/mission/deokhugam/notification/repository/NotificationRepository.java
@@ -11,23 +11,24 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 public interface NotificationRepository extends JpaRepository<Notification, UUID>,
-    NotificationRepositoryCustom {
+  NotificationRepositoryCustom {
 
 
-    // 사용자와 관련된 모든 알림을 일괄 삭제
-    @Modifying
-    @Transactional
-    @Query(value = "DELETE FROM notifications WHERE user_id IN :userIds", nativeQuery = true)
-    void deleteByUserIds(@Param("userIds") List<UUID> userIds);
+  // 사용자와 관련된 모든 알림을 일괄 삭제
+  @Modifying
+  @Transactional
+  @Query(value = "DELETE FROM notifications WHERE user_id IN :userIds", nativeQuery = true)
+  void deleteByUserIds(@Param("userIds") List<UUID> userIds);
 
-    // 삭제 대상 유저들이 작성한 리뷰와 관련된 모든 알림을 일괄 삭제 (서브쿼리 활용)
-    @Modifying
-    @Transactional
-    @Query(value = "DELETE FROM notifications WHERE review_id IN (SELECT id FROM reviews WHERE user_id IN :userIds)", nativeQuery = true)
-    void deleteByReviewUserIds(@Param("userIds") List<UUID> userIds);
+  // 삭제 대상 유저들이 작성한 리뷰와 관련된 모든 알림을 일괄 삭제 (서브쿼리 활용)
+  @Modifying
+  @Transactional
+  @Query(value = "DELETE FROM notifications WHERE review_id IN (SELECT id FROM reviews WHERE user_id IN :userIds)", nativeQuery = true)
+  void deleteByReviewUserIds(@Param("userIds") List<UUID> userIds);
 
-    // 알림 상태를 읽음으로 벌크 업데이트
-    @Modifying(clearAutomatically = true)
-    @Query("UPDATE Notification n SET n.confirmed = true WHERE n.user.id = :userId AND n.confirmed = false")
-    void updateAllAsConfirmed(@Param("userId") UUID userId);
+  // 알림 상태를 읽음으로 벌크 업데이트
+  // 업데이트 된 레코드 수를 파악하기 위해 리턴 타입을 int로 지정
+  @Modifying(clearAutomatically = true)
+  @Query("UPDATE Notification n SET n.confirmed = true WHERE n.user.id = :userId AND n.confirmed = false")
+  int updateAllAsConfirmed(@Param("userId") UUID userId);
 }
