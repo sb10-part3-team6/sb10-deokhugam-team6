0e19c4415fd57f8e14f4c071507a3d411f529ff9
LeeDyol <dlekthf819203@naver.com>
2026-04-24 14:31:07 +0900

    Docs: 유저 도메인 요청 / 응답 DTO 전체 Swagger 명세 추가

--- src/main/java/com/codeit/mission/deokhugam/user/dto/UserLoginRequest.java
+++ src/main/java/com/codeit/mission/deokhugam/user/dto/UserLoginRequest.java
@@ -1,13 +1,26 @@
 package com.codeit.mission.deokhugam.user.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
+@Schema(description = "로그인 정보")
 public record UserLoginRequest(
+    @Schema(
+        description = "이메일",
+        example = "string",
+        requiredMode = Schema.RequiredMode.REQUIRED
+
+    )
     @NotBlank(message = "이메일은 필수 입력 항목입니다.")
     @Email(message = "유효한 이메일 형식이 아닙니다.")
     String email,
 
+    @Schema(
+        description = "비밀번호",
+        example = "string",
+        requiredMode = Schema.RequiredMode.REQUIRED
+    )
     @NotBlank(message = "비밀번호는 필수 입력 항목입니다.")
     String password
 ) {
--- src/main/java/com/codeit/mission/deokhugam/user/dto/UserUpdateRequest.java
+++ src/main/java/com/codeit/mission/deokhugam/user/dto/UserUpdateRequest.java
@@ -1,9 +1,18 @@
 package com.codeit.mission.deokhugam.user.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+@Schema(description = "수정할 사용자 정보")
 public record UserUpdateRequest(
+    @Schema(
+        description = "닉네임",
+        example = "string",
+        minLength = 2,
+        maxLength = 20,
+        requiredMode = Schema.RequiredMode.REQUIRED
+    )
     @NotBlank(message = "닉네임은 필수 입력 항목입니다.")
     @Size(min = 2, max = 20, message = "닉네임은 2자 이상 20자 이하로 입력해주세요.")
     String nickname
