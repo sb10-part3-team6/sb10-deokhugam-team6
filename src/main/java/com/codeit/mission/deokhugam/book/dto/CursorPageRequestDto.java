aaeaed3e8120d9416339f475c32af47340f7ef86
초시맥북 <fyg7575@gmail.com>
2026-04-24 15:22:26 +0900

    fix: after 필드 타입 변경 Localdatetime으로

--- src/main/java/com/codeit/mission/deokhugam/book/dto/CursorPageRequestDto.java
+++ src/main/java/com/codeit/mission/deokhugam/book/dto/CursorPageRequestDto.java
@@ -2,12 +2,14 @@ package com.codeit.mission.deokhugam.book.dto;
 
 import com.codeit.mission.deokhugam.book.entity.SortDirection;
 
+import java.time.LocalDateTime;
+
 public record CursorPageRequestDto(
         String keyword,
         String orderBy,
         SortDirection direction,
         String cursor,
-        String after,
+        LocalDateTime after,
         int limit
 ) {
 }
