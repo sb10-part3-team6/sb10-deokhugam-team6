ab19bf62eafed21dc1874bd8d58bf935bb00d6f3
LeeDyol <dlekthf819203@naver.com>
2026-04-24 17:57:02 +0900

    Feat: 리뷰 좋아요 (ReviewLike) 엔티티 및 DTO 변환을 위한 MapStruct 매퍼 추가

---
+++ src/main/java/com/codeit/mission/deokhugam/review/mapper/ReviewLikeMapper.java
@@ -0,0 +1,26 @@
+package com.codeit.mission.deokhugam.review.mapper;
+
+import com.codeit.mission.deokhugam.review.dto.response.ReviewLikeDto;
+import com.codeit.mission.deokhugam.review.entity.Review;
+import com.codeit.mission.deokhugam.review.entity.ReviewLike;
+import com.codeit.mission.deokhugam.user.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface ReviewLikeMapper {
+
+  // 엔티티 변환
+  @Mapping(target = "id", ignore = true)
+  @Mapping(target = "review", source = "review")
+  @Mapping(target = "user", source = "user")
+  ReviewLike toEntity(Review review, User user);
+
+  // 엔티티 -> 응답 DTO 변환
+  @Mapping(target = "id", source = "reviewLike.id")
+  @Mapping(target = "reviewId", source = "review.id")
+  @Mapping(target = "userId", source = "user.id")
+  @Mapping(target = "liked", source = "isLiked")
+  ReviewLikeDto toDto(Review review, User user, boolean isLiked);
+}
