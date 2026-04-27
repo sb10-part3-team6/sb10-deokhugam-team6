243a0d1672f6b98483a36c804144d1231475c338
Nooroong <sjiyeon759@gmail.com>
2026-04-27 15:07:39 +0900

    Feat: 시간 타입 변경 및 공통 enum 적용

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,19 +1,4 @@
-CREATE TYPE "user_status" AS ENUM (
-    'ACTIVE',
-    'DELETED'
-    );
-
-CREATE TYPE "review_status" AS ENUM (
-    'ACTIVE',
-    'DELETED'
-    );
-
-CREATE TYPE "book_status" AS ENUM (
-    'ACTIVE',
-    'DELETED'
-    );
-
-CREATE TYPE "comment_status" AS ENUM (
+CREATE TYPE "deleted_status" AS ENUM (
     'ACTIVE',
     'DELETED'
     );
@@ -28,11 +13,11 @@ CREATE TYPE "period_type" AS ENUM (
 CREATE TABLE "users"
 (
     "id"         UUID PRIMARY KEY,
-    "email"      VARCHAR(255) UNIQUE NOT NULL UNIQUE,
+    "email"      VARCHAR(255) NOT NULL UNIQUE,
     "password"   VARCHAR(255)        NOT NULL,
     "nickname"   VARCHAR(20)         NOT NULL,
-    "status"     user_status         NOT NULL DEFAULT 'ACTIVE',
-    "created_at" TIMESTAMP           NOT NULL DEFAULT 'now()',
+    "status"     deleted_status         NOT NULL DEFAULT 'ACTIVE',
+    "created_at" TIMESTAMPtz           NOT NULL DEFAULT now(),
     "updated_at" TIMESTAMP,
     "deleted_at" TIMESTAMP
 );
@@ -47,11 +32,11 @@ CREATE TABLE "books"
     "published_date" DATE             NOT NULL,
     "isbn"           VARCHAR(20)      NOT NULL UNIQUE,
     "thumbnail_url"  VARCHAR(500),
-    "review_count"   INTEGER          NOT NULL CHECK ("review_count" >= 0),
-    "rating"         DOUBLE PRECISION NOT NULL CHECK ("rating" BETWEEN 0 AND 5),
-    "book_status"    book_status      NOT NULL DEFAULT 'ACTIVE',
+    "review_count"   INTEGER          NOT NULL DEFAULT 0 CHECK ("review_count" >= 0),
+    "rating"         DOUBLE PRECISION NOT NULL DEFAULT 0 CHECK ("rating" BETWEEN 0 AND 5),
+    "book_status"    deleted_status      NOT NULL DEFAULT 'ACTIVE',
     "deleted_at"     TIMESTAMP,
-    "created_at"     TIMESTAMP        NOT NULL DEFAULT 'now()',
+    "created_at"     TIMESTAMPtz        NOT NULL DEFAULT now(),
     "updated_at"     TIMESTAMP
 );
 
@@ -64,13 +49,13 @@ CREATE TABLE "reviews"
     "content"       TEXT          NOT NULL,
     "like_count"    INTEGER       NOT NULL DEFAULT 0,
     "comment_count" INTEGER       NOT NULL DEFAULT 0,
-    "status"        review_status NOT NULL DEFAULT 'ACTIVE',
-    "created_at"    TIMESTAMP     NOT NULL DEFAULT 'now()',
+    "status"        deleted_status NOT NULL DEFAULT 'ACTIVE',
+    "created_at"    TIMESTAMPtz     NOT NULL DEFAULT now(),
     "updated_at"    TIMESTAMP,
     "deleted_at"    TIMESTAMP,
 
-    FOREIGN KEY ("book_id") REFERENCES "books" ("id"),
-    FOREIGN KEY ("user_id") REFERENCES "users" ("id")
+    FOREIGN KEY ("book_id") REFERENCES "books" ("id") ON DELETE CASCADE,
+    FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE CASCADE
 );
 ALTER TABLE "reviews"
     ADD CONSTRAINT "uk_book_user" UNIQUE (book_id, user_id);
@@ -81,12 +66,12 @@ CREATE TABLE "comments"
     "review_id"  UUID           NOT NULL,
     "user_id"    UUID           NOT NULL,
     "content"    VARCHAR(500)   NOT NULL,
-    "status"     comment_status NOT NULL DEFAULT 'ACTIVE',
-    "created_at" TIMESTAMP      NOT NULL DEFAULT 'now()',
+    "status"     deleted_status NOT NULL DEFAULT 'ACTIVE',
+    "created_at" TIMESTAMPtz      NOT NULL DEFAULT now(),
     "updated_at" TIMESTAMP,
 
-    FOREIGN KEY ("review_id") REFERENCES "reviews" ("id"),
-    FOREIGN KEY ("user_id") REFERENCES "users" ("id")
+    FOREIGN KEY ("review_id") REFERENCES "reviews" ("id") ON DELETE CASCADE,
+    FOREIGN KEY ("user_id") REFERENCES "users" ("id")  ON DELETE CASCADE
 );
 
 CREATE TABLE "notifications"
@@ -100,21 +85,21 @@ CREATE TABLE "notifications"
     "created_at"     TIMESTAMPtz  NOT NULL DEFAULT now(),
     "updated_at"     TIMESTAMPtz,
 
-    FOREIGN KEY ("user_id") REFERENCES "users" ("id"),
-    FOREIGN KEY ("review_id") REFERENCES "reviews" ("id")
+    FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE CASCADE,
+    FOREIGN KEY ("review_id") REFERENCES "reviews" ("id")  ON DELETE CASCADE
 );
 
 CREATE TABLE "review_likes"
 (
-    "id"         UUID        NOT NULL,
+    "id"         UUID        PRIMARY KEY,
     "review_id"  UUID        NOT NULL,
     "user_id"    UUID        NOT NULL,
     "liked_at"   TIMESTAMPtz NOT NULL,
     "created_at" TIMESTAMPtz NOT NULL DEFAULT now(),
     "updated_at" TIMESTAMPtz,
 
-    FOREIGN KEY ("review_id") REFERENCES "reviews" ("id"),
-    FOREIGN KEY ("user_id") REFERENCES "users" ("id")
+    FOREIGN KEY ("review_id") REFERENCES "reviews" ("id")  ON DELETE CASCADE,
+    FOREIGN KEY ("user_id") REFERENCES "users" ("id")  ON DELETE CASCADE
 );
 ALTER TABLE "review_likes"
     ADD CONSTRAINT "uk_review_user_like" UNIQUE (review_id, user_id);
@@ -128,18 +113,18 @@ CREATE TABLE "power_users"
     "snapshot_id"      UUID             NOT NULL,
     "user_id"          UUID             NOT NULL,
     "period_type"      period_type      NOT NULL,
-    "period_start"     TIMESTAMP        NOT NULL,
-    "period_end"       TIMESTAMP        NOT NULL,
+    "period_start"     TIMESTAMPtz        NOT NULL,
+    "period_end"       TIMESTAMPtz        NOT NULL,
     "rank"             BIGINT           NOT NULL,
     "score"            DOUBLE PRECISION NOT NULL,
     "review_score_sum" DOUBLE PRECISION NOT NULL DEFAULT 0,
     "like_count"       BIGINT           NOT NULL DEFAULT 0,
     "comment_count"    BIGINT           NOT NULL DEFAULT 0,
-    "aggregated_at"    TIMESTAMP        NOT NULL DEFAULT now(),
-    "created_at"       TIMESTAMP        NOT NULL DEFAULT now(),
+    "aggregated_at"    TIMESTAMPtz        NOT NULL DEFAULT now(),
+    "created_at"       TIMESTAMPtz        NOT NULL DEFAULT now(),
     "updated_at"       TIMESTAMP,
 
-    FOREIGN KEY ("user_id") REFERENCES "users" ("id")
+    FOREIGN KEY ("user_id") REFERENCES "users" ("id")  ON DELETE CASCADE
 );
 
 ALTER TABLE "power_users"
@@ -154,18 +139,18 @@ CREATE TABLE "popular_reviews"
     "id"            UUID PRIMARY KEY,
     "review_id"     UUID             NOT NULL,
     "period_type"   period_type      NOT NULL,
-    "period_start"  TIMESTAMP        NOT NULL,
-    "period_end"    TIMESTAMP        NOT NULL,
+    "period_start"  TIMESTAMPtz        NOT NULL,
+    "period_end"    TIMESTAMPtz        NOT NULL,
     "rank"          BIGINT           NOT NULL,
     "score"         DOUBLE PRECISION NOT NULL,
     "like_count"    BIGINT           NOT NULL,
     "comment_count" BIGINT           NOT NULL,
     "snapshot_id"   UUID             NOT NULL,
-    "aggregated_at" TIMESTAMP        NOT NULL,
-    "created_at"    TIMESTAMP        NOT NULL DEFAULT now(),
+    "aggregated_at" TIMESTAMPtz        NOT NULL,
+    "created_at"    TIMESTAMPtz        NOT NULL DEFAULT now(),
     "updated_at"    TIMESTAMP,
 
-    FOREIGN KEY ("review_id") REFERENCES "reviews" ("id")
+    FOREIGN KEY ("review_id") REFERENCES "reviews" ("id")  ON DELETE CASCADE
 );
 ALTER TABLE "popular_reviews"
     ADD CONSTRAINT "uk_popular_reviews_period" UNIQUE (review_id, period_type, period_start, snapshot_id);
