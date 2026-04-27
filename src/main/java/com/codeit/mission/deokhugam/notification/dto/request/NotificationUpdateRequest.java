72b23c28baa0df98395ffddaeb65908a754e0b05
LeeDyol <dlekthf819203@naver.com>
2026-04-27 18:21:21 +0900

    Dosc: 알림 도메인 요청 / 응답 전반에 대한 Swagger 명세 추가

--- src/main/java/com/codeit/mission/deokhugam/notification/dto/request/NotificationUpdateRequest.java
+++ src/main/java/com/codeit/mission/deokhugam/notification/dto/request/NotificationUpdateRequest.java
@@ -1,8 +1,15 @@
 package com.codeit.mission.deokhugam.notification.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
+@Schema(description = "수정할 알림 상태 정보")
 public record NotificationUpdateRequest(
+    @Schema(
+        description = "상태 정보",
+        example = "true",
+        requiredMode = Schema.RequiredMode.REQUIRED
+    )
     @NotNull
     Boolean confirmed
 ) {
