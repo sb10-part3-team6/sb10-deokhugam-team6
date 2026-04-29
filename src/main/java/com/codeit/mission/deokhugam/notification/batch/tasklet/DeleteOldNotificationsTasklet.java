81b004ad390a8a0ca3e707c8d8b1b52b2da5b8bd
Nooroong <sjiyeon759@gmail.com>
2026-04-29 11:31:09 +0900

    Feat: 삭제된 알림 수 로깅 및 StepContribution에 기록

--- src/main/java/com/codeit/mission/deokhugam/notification/batch/tasklet/DeleteOldNotificationsTasklet.java
+++ src/main/java/com/codeit/mission/deokhugam/notification/batch/tasklet/DeleteOldNotificationsTasklet.java
@@ -4,6 +4,7 @@ import com.codeit.mission.deokhugam.notification.repository.NotificationReposito
 import java.time.Duration;
 import java.time.Instant;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
@@ -11,6 +12,7 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.stereotype.Component;
 
 // step 시작 시 실행되는 작업 로직 정의
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class DeleteOldNotificationsTasklet implements Tasklet {
@@ -23,7 +25,10 @@ public class DeleteOldNotificationsTasklet implements Tasklet {
     Instant cutoff = Instant.now().minus(Duration.ofDays(7));
 
     // 확인한지 1주일이 지난 알림 데이터 삭제
-    notificationRepository.deleteByConfirmedTrueAndUpdatedAtBefore(cutoff);
+    int deletedCount = notificationRepository.deleteByConfirmedTrueAndUpdatedAtBefore(cutoff);
+    
+    log.info("[DELETE_OLD_NOTIFICATIONS_TASKLET] Deleted {} old notifications", deletedCount);
+    contribution.incrementWriteCount(deletedCount);
 
     // 스텝 종료
     return RepeatStatus.FINISHED;
