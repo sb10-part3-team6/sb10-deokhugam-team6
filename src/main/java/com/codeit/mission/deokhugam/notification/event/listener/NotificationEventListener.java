e7a74f3eb234a5bfb95ef804ca688498aa473f8f
Nooroong <sjiyeon759@gmail.com>
2026-04-29 15:52:34 +0900

    Refactor: @EventListener를 @TransactionalEventListener로 교체
    - @EventListener는 이벤트가 발생하는 즉시 처리되며, 발행자와 같은 트랜잭션 내에서 실행됩니다.
    - 따라서 리스너에서 예외 발생하여 롤백이 발생할 시, 데이터가 불일치 되는 문제가 발생할 수 있습니다.
      - 예를 들어 댓글 등록에 실패하였는데 댓글 등록 알림이 생성될 수 있습니다.
    - 이를 방지하기 위헤 @TransactionalEventListener로 교체하였습니다.
    - 발행자의 트랜잭션이 성공적으로 완료된 후에 리스너가 실행되기 때문에 데이터 불일치 문제가 발생하지 않습니다.

--- src/main/java/com/codeit/mission/deokhugam/notification/event/listener/NotificationEventListener.java
+++ src/main/java/com/codeit/mission/deokhugam/notification/event/listener/NotificationEventListener.java
@@ -5,29 +5,31 @@ import com.codeit.mission.deokhugam.notification.event.ReviewLikedEvent;
 import com.codeit.mission.deokhugam.notification.event.ReviewRankedEvent;
 import com.codeit.mission.deokhugam.notification.service.NotificationService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 @RequiredArgsConstructor
 public class NotificationEventListener {
 
-    private final NotificationService notificationService;
+  private final NotificationService notificationService;
 
-    @EventListener
-    public void handleCommentRegisteredEvent(CommentRegisteredEvent event) {
-        notificationService.createByComment(event.getActorId(), event.getReceiverId(),
-            event.getReviewId());
-    }
+  // 발행자의 트랜잭션이 성공적으로 완료된 후에 이벤트가 처리되도록 함
+  @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+  public void handleCommentRegisteredEvent(CommentRegisteredEvent event) {
+    notificationService.createByComment(event.getActorId(), event.getReceiverId(),
+      event.getReviewId());
+  }
 
-    @EventListener
-    public void handleReviewLikedEvent(ReviewLikedEvent event) {
-        notificationService.createByLike(event.getActorId(), event.getReceiverId(),
-            event.getReviewId());
-    }
+  @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+  public void handleReviewLikedEvent(ReviewLikedEvent event) {
+    notificationService.createByLike(event.getActorId(), event.getReceiverId(),
+      event.getReviewId());
+  }
 
-    @EventListener
-    public void handleReviewRankedEvent(ReviewRankedEvent event) {
-        notificationService.createByReviewRanked(event.getReceiverId(), event.getReviewId());
-    }
+  @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+  public void handleReviewRankedEvent(ReviewRankedEvent event) {
+    notificationService.createByReviewRanked(event.getReceiverId(), event.getReviewId());
+  }
 }
