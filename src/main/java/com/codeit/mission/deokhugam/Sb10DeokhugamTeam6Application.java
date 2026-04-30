982cb8cef5190c03fab86997369df19316a0751a
LeeDyol <dlekthf819203@naver.com>
2026-04-30 12:19:33 +0900

    Refactor: 비동기 실행을 위한 전용 ThreadPoolTaskExecutor 및 예외 처리 설정 추가

--- src/main/java/com/codeit/mission/deokhugam/Sb10DeokhugamTeam6Application.java
+++ src/main/java/com/codeit/mission/deokhugam/Sb10DeokhugamTeam6Application.java
@@ -7,7 +7,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableAsync
 public class Sb10DeokhugamTeam6Application {
 
   public static void main(String[] args) {
