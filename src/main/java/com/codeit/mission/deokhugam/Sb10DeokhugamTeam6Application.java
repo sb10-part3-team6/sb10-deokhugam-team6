66f8f98af501b7af05f162ec21cb06c7b2d0f055
LeeDyol <dlekthf819203@naver.com>
2026-04-30 12:01:44 +0900

    Refactor: @Async 및 이벤트를 활용한 알림 생성 로직 비동기화

--- src/main/java/com/codeit/mission/deokhugam/Sb10DeokhugamTeam6Application.java
+++ src/main/java/com/codeit/mission/deokhugam/Sb10DeokhugamTeam6Application.java
@@ -2,10 +2,12 @@ package com.codeit.mission.deokhugam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
 public class Sb10DeokhugamTeam6Application {
 
   public static void main(String[] args) {
