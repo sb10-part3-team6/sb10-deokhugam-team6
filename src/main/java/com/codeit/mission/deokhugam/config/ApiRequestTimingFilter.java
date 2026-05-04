9f848a786a0ee193795a61ace4065eac2cf50354
conradrado <gimmy6707@gmail.com>
2026-05-04 21:05:04 +0900

    Feat: redis 캐싱 전후 소요시간을 알기 위해 필터 작성

---
+++ src/main/java/com/codeit/mission/deokhugam/config/ApiRequestTimingFilter.java
@@ -0,0 +1,39 @@
+package com.codeit.mission.deokhugam.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Slf4j
+@Component
+public class ApiRequestTimingFilter extends OncePerRequestFilter {
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request,
+      HttpServletResponse response,
+      FilterChain filterChain
+  ) throws ServletException, IOException {
+    long start = System.nanoTime();
+
+    try{
+      filterChain.doFilter(request, response);
+    } finally {
+      long elapsedMs = (System.nanoTime() - start) / 1_000_000;
+
+      if(request.getRequestURI().startsWith("/api")) {
+        log.info("[API_TIMING] method={}, uri={}, query={}, status={}, elapsedMs={}",
+            request.getMethod(),
+            request.getRequestURI(),
+            request.getQueryString(),
+            response.getStatus(),
+            elapsedMs
+            );
+      }
+    }
+  }
+}
